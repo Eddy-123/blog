ae7d6f366252ce69a7d2606321f44469be777375
Eddy-123 <adegnandjoueddy12@gmail.com>
2020-12-17 16:42:28 +0100

    twig service usage, instead of AbstractController

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -16,18 +16,38 @@ use App\Uploader\UploaderInterface;
 use App\Security\Voter\PostVoter;
 use App\Handler\CommentHandler;
 use App\Handler\PostHandler;
+use Twig\Environment;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
-class BlogController extends AbstractController
+class BlogController
 {
+    /**
+     * @var PostRepository
+     */
     private $repository;
+    
+    /**
+     * @var EntityManagerInterface
+     */
     private $manager;
+    
+    /**
+     * @var Environment
+     */
+    private $twig;
+
     public function __construct(
         PostRepository $repository, 
-        EntityManagerInterface $manager
+        EntityManagerInterface $manager, 
+        Environment $twig
     ) {
-        $this->manager = $manager;
         $this->repository = $repository;
+        $this->manager = $manager;
+        $this->twig = $twig;
     }
+
+    
     /**
      * @Route(
      *      "/{page}/{limit}", 
@@ -41,11 +61,11 @@ class BlogController extends AbstractController
         $posts = $this->repository->findPaginatedPosts($page, $limit);
         $pages = ceil($posts->count() / $limit);
         $range = range(max(1, $page - 3), min($page + 3, $pages));
-        return $this->render("blog/home.html.twig", [
+        return new Response($this->twig->render("blog/home.html.twig", [
             "posts" => $posts,
             "pages" => $pages,
             "range" => $range
-        ]);
+        ]));
     }
     
     /**
@@ -54,7 +74,8 @@ class BlogController extends AbstractController
     public function read(
         Post $post, 
         Request $request, 
-        CommentHandler $commentHandler
+        CommentHandler $commentHandler,
+        UrlGeneratorInterface $urlGenerator
     ): Response {
         $comment = new Comment();
         $comment->setPost($post);
@@ -68,15 +89,15 @@ class BlogController extends AbstractController
         ];
         
         if($commentHandler->handle($request, $comment, $options)){
-            return $this->redirectToRoute("blog_read", [
+            return new RedirectResponse($urlGenerator->generate("blog_read", [
                 "id" => $post->getId()
-            ]);
+            ]));
         }
 
-        return $this->render("blog/read.html.twig", [
+        return new Response($this->twig->render("blog/read.html.twig", [
             "post" => $post,
             "form" => $commentHandler->createView()
-        ]);
+        ]));
     }
     
     /**
@@ -84,7 +105,8 @@ class BlogController extends AbstractController
      */
     public function create(
         Request $request, 
-        PostHandler $postHandler
+        PostHandler $postHandler,
+        UrlGeneratorInterface $urlGenerator
     ): Response {
         $this->denyAccessUnlessGranted("ROLE_USER");
         $post = new Post();
@@ -94,14 +116,14 @@ class BlogController extends AbstractController
         ];
         
         if($postHandler->handle($request, $post, $options)){
-            return $this->redirectToRoute("blog_read", [
+            return new RedirectResponse($urlGenerator->generate("blog_read", [
                 "id" => $post->getId()
-            ]);
+            ]));
         }
             
-        return $this->render("blog/create.html.twig", [
+        return new Response($this->twig->render("blog/create.html.twig", [
             "form" => $postHandler->createView()
-        ]);
+        ]));
     }
     
     /**
@@ -110,18 +132,19 @@ class BlogController extends AbstractController
     public function update(
         Post $post, 
         Request $request, 
-        PostHandler $postHandler
+        PostHandler $postHandler,
+        UrlGeneratorInterface $urlGenerator
     ): Response {
         $this->denyAccessUnlessGranted(PostVoter::EDIT, $post);
 
         if($postHandler->handle($request, $post)){
-            return $this->redirectToRoute("blog_read", [
+            return new RedirectResponse($urlGenerator->generate("blog_read", [
                 "id" => $post->getId()
-            ]);
+            ]));
         }
         
-        return $this->render("blog/update.html.twig", [
+        return new Response($this->twig->render("blog/update.html.twig", [
             "form" => $postHandler->createView()
-        ]);
+        ]));
     }
 }
